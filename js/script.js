95b992c201c925886d9608803a49b022bc98205b
Nagai Kenta <nagaikenta@MacBook-Pro.local>
2019-10-11 00:34:25 +0900

    [完成品]

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function(){
-    var $button = $('.CDG')
+    var $button = $('.SDK')
     setTimeout(function(){
         $button.fadeIn(3000)
-    },10000)
+    },1000)
 
     setTimeout(function(){
         $('.NYA').fadeOut(1000)
@@ -11,3 +11,14 @@ $(function(){
         location.reload();
     })
 })
+
+$(function(){
+ 
+    //.accordion1の中のp要素がクリックされたら
+	$('.accordion1 p').click(function(){
+ 
+		//クリックされた.accordion1の中のp要素に隣接するul要素が開いたり閉じたりする。
+		$(this).next('ul').slideToggle();
+ 
+	});
+});
